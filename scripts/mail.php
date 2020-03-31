fe025ad83c0c936344227b72e09592e791074f8c
williamdwesp <williamdwesp@gmail.com>
2020-03-31 17:15:45 -0500

    working on mail with postfix

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,14 +1,14 @@
 <?php
 
-if(isset($_POST['mail'])){
-
-    echo "200";
-
-}
-else{
-    echo "201";    
-}
-
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "williamdwesp@gmail.com";
+$to = "williamdwesp@gmail.com";
+$subject = "PHP Mail Test script";
+$message = "This is a test to check the PHP Mail functionality";
+$headers = "From:" . $from;
+mail($to,$subject,$message, $headers);
+echo "Test email sent";
 
 
 ?>
