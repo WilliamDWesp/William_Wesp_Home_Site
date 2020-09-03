b78568a8afd297edc0dfa39578665f69668242cd
williamdwesp <williamdwesp@gmail.com>
2020-09-03 12:49:50 -0500

    Maybe the collapasable way

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ include_once('include/header.php');
 
 
 <style>
+    .jumbotron p{
+        font-size: 12pt;
+    }
+
 </style>
 
 <head>
@@ -25,7 +29,26 @@ include_once('include/header.php');
     </div>
 </div> 
 
+<div id="accordion">
+    <div class="card">
+        <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    About Me
+                </button>
+            </h5>
+        </div>
 
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+                <p class="lead">I am a Computer Science Student at Missouri Western State University. I Will graduate in May 2021. I have a love for learning and building on my skills. At Missouri Western, I was able to help Start an Association for Computer Machinery Chapter. I was vice president for two semesters and the president for one semester. Helped create a Missouri Western Linux Distribution and assisted in gaining the ability to deploy the distribution. I want to eventually get a graduate degree, preferably in cloud computing or application development, since they are the most intriguing to me. </p>
+                <p class="lead">I gain significantly from working on real problems and have gained experience working for Buchanan County as a Full Stack Intern. Working on the issues at the County has given me more confidence and gained better problem-solving skills. </p>
+                <p class="lead">I tend to enjoy exploring new technologies and keeping up with new software and hardware in my free time. I love playing video games and have been an avid gamer since I was in first grade. I always have music going, and if I don't, I tend to hum a song to myself. Having that noise helps me focus and just program.</p>
+            </div>
+        </div>
+
+    </div>
+</div>
 
 
 
