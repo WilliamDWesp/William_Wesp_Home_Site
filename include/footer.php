f80383efcd1f52866484d546d6e79b46d2823179
williamdwesp <williamdwesp@gmail.com>
2020-09-03 13:40:58 -0500

    Working on Jquery buttons

--- include/footer.php
+++ include/footer.php
@@ -4,19 +4,19 @@
         
         <div class="container">
             <div class="row">
-                <a class="btn btn-social btn-github " href="https://github.com/WilliamDWesp">
+                <a id="git" class="btn btn-social btn-github ">
                     <span class="fa fa-github"></span> Find me on  github
                 </a>
-                <a class="btn btn-social btn-linkedin" href="https://www.linkedin.com/in/william-wesp-865069177/">
+                <a id="linked" class="btn btn-social btn-linkedin" >
                     <span class="fa fa-linkedin"></span> Find me on  linkedin
                 </a>
             </div>
             <div class="row">
                 <a class="">
-                    <span class=""></span> Email: williamdwesp@gmail.com or wwesp@missouriwestern.edu
+                    <span class=""></span>    Email: williamdwesp@gmail.com or wwesp@missouriwestern.edu
                 </a>
                 <a class="n">
-                    <span class=""></span> Phone: +(1) 303-883-7765
+                    <span class=""></span>    Phone: +(1) 303-883-7765
                 </a>
             </div>
         </div>
@@ -27,5 +27,7 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <!-- Custom js-->
+        <script src="/scripts/JS/FooterButtons.js"></script>
     </body>
 </html>
---
+++ scripts/JS/FooterButtons.js
@@ -0,0 +1,5 @@
+$(#git).click(function() {
+    alert( "Handler for .click() called." );
+});
+///"https://github.com/WilliamDWesp"
+//"https://www.linkedin.com/in/william-wesp-865069177/"
